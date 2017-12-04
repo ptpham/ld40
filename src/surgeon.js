3bb098a4fdc5d6ce8a99bd3ceaf4ecdae0d4096f
Cindy Zhang <cixzhang@gmail.com>
2017-12-03 17:49:59 -0800

    adding under eye surgeries

--- src/surgeon.js
+++ src/surgeon.js
@@ -53,6 +53,36 @@ var surgeries = {
       },
     };
   },
+  'under eye enlargement': (skill, heal) => {
+    var jitter = _.random(0, 2 - skill);
+    return {
+      normalShifts: {
+        under_eyes: 0.1 + _.random(0, 0.05 * jitter, true),
+        upper_cheek_right: _.random(0, 0.05 * jitter, true),
+        upper_cheek_left: _.random(0, 0.05 * jitter, true),
+      },
+      injuryValues: {
+        under_eyes: heal * HEAL_PER_TURN,
+        upper_cheek_right: jitter * HEAL_PER_TURN,
+        upper_cheek_left: jitter * HEAL_PER_TURN,
+      },
+    };
+  },
+  'under eye reduction': (skill, heal) => {
+    var jitter = _.random(0, 2 - skill);
+    return {
+      normalShifts: {
+        under_eyes: -0.1 + _.random(0, 0.05 * jitter, true),
+        upper_cheek_right: _.random(0, 0.05 * jitter, true),
+        upper_cheek_left: _.random(0, 0.05 * jitter, true),
+      },
+      injuryValues: {
+        under_eyes: heal * HEAL_PER_TURN,
+        upper_cheek_right: jitter * HEAL_PER_TURN,
+        upper_cheek_left: jitter * HEAL_PER_TURN,
+      },
+    };
+  },
   'cheek lift': (skill, heal) => {
     var jitter = _.random(0, 2 - skill);
     return {
