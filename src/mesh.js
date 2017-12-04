718f8c9d754aa33af54ccdfbd93172e70d22712b
Peter Pham <ptr.pham@gmail.com>
2017-12-03 16:23:16 -0800

    Adding new face parts.

--- src/mesh.js
+++ src/mesh.js
@@ -11,15 +11,25 @@ let faceWeights = _.mapValues(require('../mesh/faceWeights.json'),
 
 let displayNamePairs = [
   ['nose_bridge', 'nose bridge'],
+  ['nose_nostrils', 'nostrils'],
+  ['nose_tip', 'nose tip'],
   ['upper_lip_center', ,'upper lip'],
   ['lower_lip_center', 'lower lip'],
   ['upper_ear_left', 'upper left ear'],
+  ['ear_lobe_left', 'left ear lobe'],
   ['upper_ear_right', 'upper right ear'],
+  ['ear_lobe_right', 'right ear lobe'],
   ['under_eyes', 'area under your eyes'],
+  ['eye_lids', 'eye lids'],
   ['upper_cheek_left', 'upper left cheek'],
+  ['lower_cheek_left', 'lower left cheek'],
   ['upper_cheek_right', 'upper right cheek'],
+  ['lower_cheek_right', 'lower right cheek'],
   ['brow_left', 'left brow'],
-  ['brow_right', 'right brow']
+  ['brow_right', 'right brow'],
+  ['chin_center', 'chin'],
+  ['jaw_left', 'left jaw'],
+  ['jaw_right', 'right jaw'],
 ];
 
 let partDisplayNameMap = new Map(displayNamePairs);
